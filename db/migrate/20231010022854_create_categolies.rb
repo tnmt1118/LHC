ec463cb9256eb3ed080c91a242e0778088c1caae
tnmt <tnmt@example.com>
2023-10-10 11:44:51 +0900

    scaffold終わった

--- db/migrate/20231010022854_create_categolies.rb
+++ db/migrate/20231010022854_create_categolies.rb
@@ -1,7 +1,7 @@
 class CreateCategolies < ActiveRecord::Migration[6.1]
   def change
     create_table :categolies do |t|
-      t.string :api-category
+      t.string :api_category
       t.string :category1
       t.string :category2
       t.string :category3
