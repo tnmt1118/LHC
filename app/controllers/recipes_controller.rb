3899cab6a5dab0346a75be6e4c1cc9301dd56a05
tnmt <tnmt@example.com>
2023-10-31 11:01:12 +0900

    wwww

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,9 @@
 class RecipesController < ApplicationController
   def display
+    uri = URI.parse("https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=#{Settings.rakuten.applicationid}")
+    json = Net::HTTP.get(uri)
+    @rec = JSON.parse(json)
+    
   end
 
   def details
