addb304d6053c3d1b300ca3b9eb4fcbb308b393b
tnmt <tnmt@example.com>
2023-10-31 11:40:48 +0900

    表示

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,8 @@ class RecipesController < ApplicationController
     uri = URI.parse("https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=#{Settings.rakuten.applicationid}")
     json = Net::HTTP.get(uri)
     @rec = JSON.parse(json)
-    
+    @rec_foodImageUrl = @rec["result"][0]["foodImageUrl"]
+    #puts rec ["result"]["small"][0]["recipeTitle"]
   end
 
   def details
