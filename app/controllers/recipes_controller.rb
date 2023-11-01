09362c5530b3de8e4655b79f2fe072fa8111b845
3410 <3410@example.com>
2023-11-01 12:46:29 +0900

    うい

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,12 @@
 class RecipesController < ApplicationController
   def display
-    uri = URI.parse("https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=#{Settings.rakuten.applicationid}")
+    uri = URI.parse("https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?applicationId=#{Settings.rakuten.applicationid}")
     json = Net::HTTP.get(uri)
     @rec = JSON.parse(json)
-    @rec_foodImageUrl = @rec["result"][0]["foodImageUrl"]
-    @rec_recipeTitle = @rec["result"][0]["recipeTitle"]
-    @rec_recipeIndication = @rec["result"][0]["recipeIndication"]
-    @rec_recipeCost = @rec["result"][0]["recipeCost"]
+    #@rec_foodImageUrl = @rec["result"][0]["foodImageUrl"]
+    #@rec_recipeTitle = @rec["result"][0]["recipeTitle"]
+    #@rec_recipeIndication = @rec["result"][0]["recipeIndication"]
+    #@rec_recipeCost = @rec["result"][0]["recipeCost"]
     #puts rec ["result"]["small"][0]["recipeTitle"]
   end
 
